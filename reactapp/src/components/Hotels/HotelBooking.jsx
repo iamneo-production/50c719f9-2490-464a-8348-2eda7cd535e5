76089623a76e3bd02fb638d8c62f1ff7ee46827d
Rajashekar2904 <rajashekaryadav8630@gmail.com>
2023-07-11 18:49:19 +0530

    added in booking part

--- reactapp/src/components/Hotels/HotelBooking.jsx
+++ reactapp/src/components/Hotels/HotelBooking.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import { DatePicker} from "antd";
 import Styles from "./ViewHotel.module.css"
 import "./ViewHotel.css"
 const HotelBooking = () => {
-  return (
+  const [data,setData]=useState([]);
+  useEffect(()=>{
+    fetch("http://localhost:8084/cabdetail/cabs")
+    .then(res=>res.json())
+    .then((result)=>{
+        setData(result);
+    console.log(data)})
+        .catch(error=>console.error(error));
+},[])
+    return (
     <div className='container'>
         <div className='row'>
         <h3>Hotel Kohinoor</h3>
             <div className='col-lg-12 d-flex flex-row justify-content-around mt-5 card'>
                 <div>
                 <p>Check In</p>
-                <p>30th May 2023</p>
+                
+                <DatePicker/>
                 </div>
                 <div>
                 <p>Check Out</p>
-                <p>30th May 2023</p>
+                <DatePicker/>
+                
                 </div>
                 <div>
                     <p>1 Night | 2 Adults | 1 Room</p>
@@ -35,37 +47,44 @@ const HotelBooking = () => {
         <div className={`col-lg-8 ${Styles.guestDetails}`}>
                 <div className="mt-3">
                     
-                <label>Name</label>
-                    <input type="text" className="form-control col-lg-12 mb-3" placeholder='enter your Name'/>
+                <label>Name:</label>
+                    <input type="text" className="form-control col-lg-12 mb-3" placeholder='Enter your Name'/>
                 </div>
                 <div>
+                    <p>Gender:</p>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="name" />
+                        <label class="form-check-label">Male</label>
+                    </div>
+
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="name" />
+                        <label class="form-check-label">Female</label>
+                    </div>
+                </div><br></br>
+                <div>
                     
-                <label>Email</label>
-                <input type="text" className="form-control col-lg-12 mb-3" placeholder='enter your Email'
+                <label>Email:</label>
+                <input type="text" className="form-control col-lg-12 mb-3" placeholder='Enter your Email'
                 />
 
                 </div>
                 <div>
     
-                <label>Mobile</label>
-                <input type="text" className="form-control col-lg-12 mb-3" placeholder='enter your Mobile Number'/>
+                <label>Mobile:</label>
+                <input type="text" className="form-control col-lg-12 mb-3" placeholder='Enter your Mobile Number'/>
 
                 </div>
 
-                {/* <div className='my-4'>
-                    <label>Select Payment Method</label>
-                    <br/>
-                    <input type="radio" name="phonePay" /> Phone Pay &nbsp;&nbsp;&nbsp;
-                    <input type="radio" name="phonePay" /> Google Pay&nbsp;&nbsp;&nbsp;
-                    <input type="radio" name="phonePay" /> Net Banking  
-                </div> */}
 
                 <div className='my-5'>
-                    <button className={`btn ${Styles.bookNow}`}>Book Now</button>
+                    <button className={`btn ${Styles.bookNow}`}>Confirm Booking</button>
                 </div>
         </div>
 
-
+{data.map(HotelBooking=>(<li key={HotelBooking.id}>
+    id:{HotelBooking.nbr}
+</li>))}
     </div>
   )
 }
